af53f409065ecf2b49cb928030404830104ee8f1
Mikhail Molodozhenov (Mishamol) <mishamol@bk.ru>
2023-12-23 13:15:44 +0300

    Scenes & Scripts rename

--- nodes/ui/scripts/button_switch_scene.gd
+++ nodes/ui/scripts/button_switch_scene.gd
@@ -1,11 +1,11 @@
 extends TextureButton
 
 
-@export_file var level_path
+@export var level: PackedScene
 
 
 func _on_pressed():
-	if level_path == null:
+	if level == null:
 		return
 	get_tree().set_deferred("paused", false)
-	get_tree().change_scene_to_file(level_path)
+	get_tree().change_scene_to_packed(level)
