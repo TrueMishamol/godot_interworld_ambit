8a473b00965ce5eff880ab7aa63600cacd39acb5
Mikhail Molodozhenov (Mishamol) <mishamol@bk.ru>
2023-12-24 11:28:34 +0300

    Scene Path @export => @export_file

--- levels/level_info_resource.gd
+++ levels/level_info_resource.gd
@@ -3,6 +3,10 @@ extends Resource
 class_name LevelInfoResource
 
 @export var Name: String = ""
+#@export_file("*.tscn","*.scn") var Scene: String = ""
+#@export_file("*.tscn","*.scn") var Scene: String = ""
 #@export var Scene: PackedScene = null
-@export var Path: String = "res://levels/menu/main_menu.tscn"
+@export_file("*.tscn","*.scn") var Path: String = "res://levels/menu/main_menu.tscn"
 @export var Description: String = ""
+
+#@export_group(String, FILE, "*.tscn,*.scn") var file_path setget set_file_path
