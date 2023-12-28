6058fd1593692f4b5c0ce6a0b3e2afc7f4827035
Mikhail Molodozhenov (Mishamol) <mishamol@bk.ru>
2023-12-28 22:29:15 +0300

    Add Mishamol dialogue & setup Interface

--- levels/chapter_1/scripts/level.gd
+++ levels/chapter_1/scripts/level.gd
@@ -1,9 +1,13 @@
 extends Node2D
 
 
-@onready var _dialogue_box = $DialogueBox
+@onready var _dialogue_box = %DialogueBox
 
 
 func _ready():
+	#if not dialogue_box.running:
+		#dialogue_box.start()
+	
 	if !_dialogue_box.running:
 		_dialogue_box.start("MISHAMOL")
+		print ("dialogue should run")
